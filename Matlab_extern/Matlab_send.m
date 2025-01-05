841af2c188d402381312440cb652fb20f2819c6c
lukasmundorf <lukasm1911@gmail.com>
2025-01-05 17:06:42 +0100

    Change Protocol from string to Json, submit Time Series instead of one single datapoint

--- Matlab_extern/Matlab_send.m
+++ Matlab_extern/Matlab_send.m
@@ -4,18 +4,38 @@
 %mqttClient = mqttclient("tcp://localhost",ClientID="",Port=1884)
 mqttClient = mqttclient("tcp://localhost:1884");
 
-% Nachricht (Zahl 8) definieren
-message = '299';
-
 % Topic definieren
 topic = 'test/topic';
 
-% Nachricht veröffentlichen
-write(mqttClient, topic, message);
+% Endlosschleife für den kontinuierlichen Versand
+disp('Start des kontinuierlichen MQTT-Datenversands...');
+try
+    i = 1; % Initialisierung des Zählers
+    while i<21
+        % Einfache Daten generieren
+        timeStamp = posixtime(datetime('now')); % Unix-Zeitstempel
+        sampleValue = i; % Beispiel-Daten: hochzählender Zähler
 
+        % Daten in Struktur speichern
+        output = struct('time', timeStamp, 'value', sampleValue);
 
+        % Daten in JSON konvertieren
+        payload = jsonencode(output);
 
-% Client löschen
-clear mqttClient;
+        % JSON-Daten an den Broker senden
+        write(mqttClient, topic, payload);
+        disp(['Daten gesendet: ' payload]);
 
-disp('Nachricht "8" wurde erfolgreich an den MQTT-Broker gesendet.');
+        % Pause für 1 Sekunde
+        pause(1);
+
+        % Zähler erhöhen
+        i = i + 1;
+    end
+catch ME
+    disp('Skript wurde gestoppt.');
+    disp(['Fehler: ' ME.message]);
+end
+
+disp('MQTT-Datenversand abgeschlossen.');
+
