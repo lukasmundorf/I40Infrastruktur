8f4cec866739903cdcd1d4aef35b808608498404
lukasmundorf <lukasm1911@gmail.com>
2025-01-06 11:00:27 +0100

    Hinzufügen eines unabhängigen Sinussignals als fiktiver Sensor. Schnellere Abtastrate, allerdings noch nicht optimiert

--- Matlab_extern/Matlab_send.m
+++ Matlab_extern/Matlab_send.m
@@ -4,22 +4,44 @@ mqttClient = mqttclient("tcp://localhost:1884");
 % Topic definieren
 topic = 'test/topic';
 
-% Endlosschleife für den kontinuierlichen Versand
-disp('Start des kontinuierlichen MQTT-Datenversands...');
-try
-    i = 1; % Initialisierung des Zählers
-    while i < 21
-        % Einfache Daten für zwei Sensoren generieren
-        timeStamp = posixtime(datetime('now')); % Unix-Zeitstempel
-        sensor1Value = i * 1.1; % Beispiel-Daten für Sensor1
-        sensor2Value = i * 2.2; % Beispiel-Daten für Sensor2
+% Parameter für das Sinussignal
+samplingRate = 100; % Abtastrate in Hz
+signalFrequency1 = 0.5; % Frequenz für Sensor1 in Hz
+signalFrequency2 = 0.3; % Frequenz für Sensor2 in Hz
+amplitude1 = 1; % Amplitude für Sensor1
+amplitude2 = 2; % Amplitude für Sensor2
+dt = 1 / samplingRate; % Abtastintervall
 
-         % Sensor1-Daten
+% Geschlossene Funktion (Closure) für das Signal-Update erstellen
+signalUpdater = createSignalUpdater(amplitude1, signalFrequency1, amplitude2, signalFrequency2, dt);
+
+% Start des kontinuierlichen Sinus-Signals
+disp('Start der Signal- und MQTT-Datenübertragung...');
+try
+    % Timer für das Sinussignal
+    signalTimer = timer(...
+        'ExecutionMode', 'fixedRate', ...
+        'Period', dt, ...
+        'TimerFcn', @(~,~) signalUpdater(true));
+
+    % Timer starten
+    start(signalTimer);
+
+    % Endlosschleife für das Senden der Daten
+    i = 1;
+    while i <= 200
+        % Einfache Daten generieren (Zeitstempel hinzufügen)
+        timeStamp = posixtime(datetime('now')); % Unix-Zeitstempel
+
+        % Aktuelle Sensorwerte abrufen
+        [sensor1Value, sensor2Value] = signalUpdater();
+
+        % Sensor1-Daten
         sensor1Data = struct('time', timeStamp, ...
                              'Sensor1', sensor1Value, ...
                              'location', 'Room1', ...
                              'device', 'DeviceA');
-        
+
         % Sensor2-Daten
         sensor2Data = struct('time', timeStamp, ...
                              'Sensor2', sensor2Value, ...
@@ -36,15 +58,46 @@ try
         write(mqttClient, topic, payload2);
         disp(['Daten für Sensor2 gesendet: ' payload2]);
 
-        % Pause für 1 Sekunde
-        pause(1);
+        % Pause für 1 Sekunde zwischen den Sendungen
+        pause(0.1);
 
         % Zähler erhöhen
         i = i + 1;
     end
+
+    % Timer stoppen
+    stop(signalTimer);
+    delete(signalTimer);
+
 catch ME
     disp('Skript wurde gestoppt.');
-    disp(['Fehler: ' ME.message]);
+    disp(['Fehler: ' ME.message']);
 end
 
 disp('MQTT-Datenversand abgeschlossen.');
+
+% Funktion zum Erstellen des Signal-Updaters
+function updater = createSignalUpdater(amplitude1, freq1, amplitude2, freq2, dt)
+    % Geschlossene Variablen
+    t = 0; % Startzeit
+    sensor1Value = 0;
+    sensor2Value = 0;
+
+    % Aktualisierungsfunktion
+    updater = @updateSignal;
+
+    function [s1, s2] = updateSignal(incrementTime)
+        if nargin > 0 && incrementTime
+            % Zeitvariable nur aktualisieren, wenn explizit angefordert
+            t = t + dt;
+        end
+
+        % Sinus-Signale generieren
+        sensor1Value = amplitude1 * sin(2 * pi * freq1 * t);
+        sensor2Value = amplitude2 * sin(2 * pi * freq2 * t);
+
+        % Rückgabe der aktuellen Sensorwerte
+        s1 = sensor1Value;
+        s2 = sensor2Value;
+    end
+end
