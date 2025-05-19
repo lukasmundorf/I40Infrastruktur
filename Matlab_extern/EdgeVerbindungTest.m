8e5c5342ade38b398176eff7d650425aedd1778f
lukasmundorf <uwmet@student.kit.edu>
2025-05-19 15:00:16 +0200

    Deleted unneccesary Code

--- Matlab_extern/EdgeVerbindungTest.m
+++
@@ -1,5 +0,0 @@
-mqttClient = mqttclient("tcp://localhost:9883", ClientID="mqttx_017e530e", Username="DMCMaschine", Password="DMCMaschine");
-%mqttClient = mqttclient("mqtt://172.22.168.64", ClientID="mqttx_017e530e",Port = 9883, Username="edge", Password="edge");
-
-disp(mqttClient.Connected);
-topic = "test/control";    % Für Steuerbefehle
